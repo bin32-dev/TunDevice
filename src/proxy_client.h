ba452236fb55e8768a77d2c9550a802af5a51ff5
bin32-dev <sabbirb2528@gmail.com>
2026-03-12 06:25:22 -0400

    Implement TUN packet forwarding loop with live logs

--- src/proxy_client.h
+++ src/proxy_client.h
@@ -2,14 +2,24 @@
 #define TUN_PROXY_CLIENT_H
 
 #include <stddef.h>
+#include <stdint.h>
 
-int proxy_send_request(const char* host,
-                       unsigned short port,
-                       const char* request,
-                       char* response,
-                       size_t response_size,
-                       char* error,
-                       size_t error_size);
+int proxy_open_connection(const char* host,
+                          unsigned short port,
+                          int* out_sock,
+                          char* error,
+                          size_t error_size);
+
+int proxy_forward_packet(int sock,
+                         const uint8_t* packet,
+                         size_t packet_size,
+                         uint8_t* response,
+                         size_t response_capacity,
+                         size_t* response_size,
+                         char* error,
+                         size_t error_size);
+
+void proxy_close_connection(int* sock);
 
 unsigned int packet_checksum32(const unsigned char* data, size_t len);
 
